fea0074527b612030a01b9609cfdece11f821aec
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-07-10 14:01:17 -0400

    initial request functions added

---
+++ requesterApp/api/__init__.py
@@ -0,0 +1,1 @@
+import requesterApp.api.makeRequests
---
+++ requesterApp/api/makeRequests.py
@@ -0,0 +1,92 @@
+import flask
+import requests
+import requesterApp
+
+MAX_ARTICLES = 250
+STRIPPED = lambda s: "".join(i for i in s if 31 < ord(i) < 127)
+
+def addSourceCountry(searchQuery, country):
+    return searchQuery + ' sourcecountry:' + country['id']
+
+def createDateStr(inDate, inTime):
+
+    dateParts = inDate.split('/')
+    month = dateParts[0]
+    day = dateParts[1]
+    year = dateParts[2]
+
+    timeParts = inTime.split(':')
+    hours = timeParts[0]
+    minutes = timeParts[1]
+    seconds = timeParts[2]
+
+    queryDate = year + month + day + hours + minutes + seconds
+
+    return queryDate
+
+# cleans Response from GDELT
+def gdeltCleanResp(resp):
+
+    try:
+        results = resp.json()
+    except json.decoder.JSONDecodeError:
+        firstStrip = re.sub('\\\\', '', resp.text)
+        correctStr = STRIPPED(firstStrip)
+        
+        try:
+            results = json.loads(correctStr)
+        except:
+            return None
+
+    return results
+
+def gdeltAPICall(payload):
+
+    # make GDELT API call
+    gdeltURL = 'https://api.gdeltproject.org/api/v2/doc/doc'
+
+    resp = requests.get(gdeltURL, params=payload)
+    processedResp = gdeltCleanResp(resp)
+
+    if processedResp == None:
+        raise Exception('RESULTS COULD NOT BE CLEANED')
+    
+    return processedResp
+
+def getArtList(req):
+
+    # req = flask.request.json
+    reqList = req["requests"]
+
+    fullResp = []
+
+    for i in range(len(reqList)):
+        currReq = reqList[i]
+        fullQuery = addSourceCountry(currReq[0], currReq[1])
+
+        # builds payload for GDELT request
+        payload = {}
+        payload['QUERY'] = fullQuery
+        payload['MODE'] = 'ArtList'
+        payload['FORMAT'] = 'JSON'
+        payload['MAXRECORDS'] = MAX_ARTICLES
+        payload['STARTDATETIME'] = createDateStr(currReq[2], currReq[3])
+        payload['ENDDATETIME'] = createDateStr(currReq[4], currReq[5])
+
+        articleResults = []
+        apiResp = gdeltAPICall(payload)
+        if len(api.keys()) > 0:
+            articles = apiResp["articles"]
+            
+            if len(articles) >= MAX_ARTICLES:
+                articleResults.append("granular")
+                articleResults.append(currReq)
+            else:
+                articleResults.append("results")
+                articleResults.append(articles)
+        else:
+            articleResults.append("none")
+        
+        fullResp.append(articleResults)
+
+    return fullResp
