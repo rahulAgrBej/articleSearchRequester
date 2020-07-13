f360d780a76b183ee35e1816efab6839196ac647
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-07-13 13:54:24 -0400

    testing request parameters being sent

--- requesterApp/api/manageRequests.py
+++ requesterApp/api/manageRequests.py
@@ -4,10 +4,12 @@ import requesterApp
 from requesterApp.api import makeRequests
 
 
-@requesterApp.app.route("/api/sendReqs", methods=["GET"]
+@requesterApp.app.route("/api/sendReqs", methods=["GET"])
 def receiveReqs():
     reqs = flask.request.json
+    """
     results = makeRequests.getArtList(reqs)
     resp = {}
     resp["response"] = results
-    return flask.jsonify(**resp)
+    """
+    return flask.jsonify(**reqs)
