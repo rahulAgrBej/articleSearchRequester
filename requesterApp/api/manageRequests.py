cda18f86c7028f648d21f1a85b21686d05eabea6
rahulAgrBej <rahul.agrawal.b@gmail.com>
2020-07-13 14:08:11 -0400

    trying to see if it will make gdelt reqs

--- requesterApp/api/manageRequests.py
+++ requesterApp/api/manageRequests.py
@@ -7,11 +7,7 @@ from requesterApp.api import makeRequests
 @requesterApp.app.route("/api/sendReqs", methods=["GET"])
 def receiveReqs():
     reqs = flask.request.args.get("reqListSent")
-    resp = {}
-    resp["received"] = reqs
-    """
     results = makeRequests.getArtList(reqs)
     resp = {}
     resp["response"] = results
-    """
     return flask.jsonify(**resp)
